8109e86218ef2d549ef0823f2bdba857b0b30021
Justin Stewart <stewartjustin3@gmail.com>
2016-05-14 15:07:39 -0400

    Finished adding reset button

---
+++ js/app.js
@@ -0,0 +1,25 @@
+angular.module('tipApp', [])
+.controller('TipCtrl', function($scope){
+	$scope.mealPrice = 100;
+	$scope.taxRate = 10;
+	$scope.tipPercentage = 20;
+	$scope.counter = 0;
+	$scope.tipTotal = 0;
+	$scope.tipSubmit = function(){
+		$scope.subTotal = ($scope.mealPrice * ($scope.taxRate/100)) + $scope.mealPrice;
+		$scope.tip = ($scope.subTotal * ($scope.tipPercentage/100));
+		$scope.total = $scope.subTotal + $scope.tip;
+		$scope.counter++;
+		$scope.tipTotal = $scope.tip + $scope.tipTotal;
+		$scope.tipAverage = $scope.tipTotal/$scope.counter; 
+    };
+
+    $scope.reset = function(){
+    	$scope.counter = 0;
+    	$scope.tipTotal = 0;
+    	$scope.subTotal = 0;
+    	$scope.tip = 0;
+    	$scope.total = 0;
+    	$scope.tipAverage = 0;
+    }
+});
