7050b8ccd4af43783a7ed3989d037aa59f445c72
Justin Stewart <stewartjustin3@gmail.com>
2016-05-17 07:19:34 -0400

    finished waitstaff

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 angular.module('tipApp', [])
 .controller('TipCtrl', function($scope){
-	$scope.mealPrice = 100;
-	$scope.taxRate = 10;
-	$scope.tipPercentage = 20;
+	// $scope.mealPrice = 100;
+	// $scope.taxRate = 10;
+	// $scope.tipPercentage = 20;
 	$scope.counter = 0;
 	$scope.tipTotal = 0;
-	$scope.tipSubmit = function(){
+	$scope.submit = function(){
 		$scope.subTotal = ($scope.mealPrice * ($scope.taxRate/100)) + $scope.mealPrice;
 		$scope.tip = ($scope.subTotal * ($scope.tipPercentage/100));
 		$scope.total = $scope.subTotal + $scope.tip;
@@ -14,12 +14,11 @@ angular.module('tipApp', [])
 		$scope.tipAverage = $scope.tipTotal/$scope.counter; 
     };
 
+    $scope.clear = function(){
+    	$scope.mealPrice= 0; $scope.taxRate= 0; $scope.tipPercentage = 0;
+    }
+
     $scope.reset = function(){
-    	$scope.counter = 0;
-    	$scope.tipTotal = 0;
-    	$scope.subTotal = 0;
-    	$scope.tip = 0;
-    	$scope.total = 0;
-    	$scope.tipAverage = 0;
+    	$scope.counter= 0; $scope.tipTotal=0; $scope.subTotal=0; $scope.tip=0; $scope.total=0; $scope.tipAverage=0;
     }
 });
